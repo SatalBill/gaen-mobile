6d1af293137211e76c27bf2ab478aacc94b6f149
Devin Jameson <devinjameson@me.com>
2020-08-06 13:59:51 -0400

    wip

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,19 +1,62 @@
-import React from "react"
-import { StyleSheet, ImageBackground, View } from "react-native"
+import React, { FunctionComponent } from "react"
+import { Alert, Linking, StyleSheet, ImageBackground, View } from "react-native"
 import { SvgXml } from "react-native-svg"
 import { useTranslation } from "react-i18next"
+import env from "react-native-config"
 
 import { usePermissionsContext } from "../PermissionsContext"
 import { useStatusBarEffect } from "../navigation"
-import Home from "./Home"
+import { isPlatformiOS } from "../utils/index"
+import { ENPermissionStatus } from "../PermissionsContext"
+import { GlobalText } from "../components/GlobalText"
+import { Button } from "../components/Button"
 
 import { Icons, Images } from "../assets"
-import { Spacing, Layout } from "../styles"
+import { Spacing, Colors, Typography, Layout } from "../styles"
 
-const HomeScreen = (): JSX.Element => {
+const HomeScreen: FunctionComponent = () => {
   useStatusBarEffect("light-content")
   const { exposureNotifications } = usePermissionsContext()
   const { t } = useTranslation()
+
+  const [
+    authorization,
+    enablement,
+  ]: ENPermissionStatus = exposureNotifications.status
+  const isEnabled = enablement === "ENABLED"
+  const isAuthorized = authorization === "AUTHORIZED"
+  const isEnabledAndAuthorized = isEnabled && isAuthorized
+
+  const showUnauthorizedAlert = () => {
+    Alert.alert(
+      t("home.bluetooth.unauthorized_error_title"),
+      t("home.bluetooth.unauthorized_error_message"),
+      [
+        {
+          text: t("common.settings"),
+          onPress: () => Linking.openSettings(),
+        },
+      ],
+    )
+  }
+
+  const handleRequestPermission = () => {
+    if (isAuthorized) {
+      exposureNotifications.request()
+    } else if (isPlatformiOS()) {
+      showUnauthorizedAlert()
+    }
+  }
+
+  const appName = env.IN_APP_NAME || "PathCheck"
+
+  const headerText = isEnabledAndAuthorized
+    ? appName
+    : t("home.bluetooth.tracing_off_header")
+  const subheaderText = isEnabledAndAuthorized
+    ? t("home.bluetooth.all_services_on_subheader")
+    : t("home.bluetooth.tracing_off_subheader")
+  const buttonText = t("home.bluetooth.tracing_off_button")
 
   return (
     <ImageBackground
@@ -30,10 +73,24 @@ const HomeScreen = (): JSX.Element => {
         />
       </View>
       <View style={style.homeContainer}>
-        <Home
-          enPermissionStatus={exposureNotifications.status}
-          requestPermission={exposureNotifications.request}
-        />
+        <View style={style.container}>
+          <>
+            <GlobalText style={style.headerText} testID={"home-header"}>
+              {headerText}
+            </GlobalText>
+            <GlobalText style={style.subheaderText} testID={"home-subheader"}>
+              {subheaderText}
+            </GlobalText>
+          </>
+          {!isEnabledAndAuthorized ? (
+            <Button
+              invert
+              testID={"home-request-permissions-button"}
+              onPress={handleRequestPermission}
+              label={buttonText}
+            />
+          ) : null}
+        </View>
       </View>
     </ImageBackground>
   )
@@ -52,8 +109,26 @@ const style = StyleSheet.create({
   },
   homeContainer: {
     flex: 1,
-    padding: Spacing.medium,
+  },
+  container: {
+    justifyContent: "flex-end",
+    backgroundColor: Colors.primaryBackground,
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+  },
+  headerText: {
+    ...Typography.xLargeFont,
+    ...Typography.bold,
+    lineHeight: Typography.mediumLineHeight,
+    textAlign: "center",
+  },
+  subheaderText: {
+    ...Typography.header4,
+    textAlign: "center",
+    marginTop: Spacing.medium,
   },
 })
 
 export default HomeScreen
+
